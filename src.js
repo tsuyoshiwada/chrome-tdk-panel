7c0544e717ef36c7549141a995cdb09dcf459408
tsuyoshiwada <v23.b.wada@gmail.com>
2016-01-07 01:16:13 +0900

    Update redesign

--- src.js
+++ src.js
@@ -5,7 +5,7 @@
     return document.querySelector(selector);
   }
 
-  function className(str) {
+  function p(str) {
     return ID + str;
   }
 
@@ -16,10 +16,8 @@
   }
 
   function createRowElement(title, content) {
-    const $el = createElement("div", ```
-    <strong>${title}</strong><span>${content}</span>
-    ```);
-    $el.className = className("row");
+    const $el = createElement("div", ` <strong>${title}</strong><span>${content}</span>`);
+    $el.className = p("row");
     return $el;
   }
 
@@ -34,19 +32,53 @@
     const $description = $("meta[name='description']");
     const $keywords = $("meta[name='keywords']");
 
-    $panel.innerHTML = ```
+    $panel.innerHTML = `
     <style>
     #${ID} {
       position: fixed;
+      top: 5px;
+      right: 5px;
+      z-index: 999999;
+      padding: 5px 20px;
+      background: rgba(38, 50, 56, .96);
+      border-radius: 3px;
+      box-shadow: 0 0 5px rgba(0, 0, 0, .5);
+    }
+    #${p("close")} {
+      position: absolute;
       top: 0;
       right: 0;
-      z-index: 10000;
-      background: rgba(38, 50, 56, 0.9);
+      width: 20px;
+      height: 20px;
+      color: #fff;
+      font-size: 15px;
+      font-weight: bold;
+      font-family: sans-serif;
+      line-height: 18px;
+      text-align: center;
+      cursor: pointer;
+    }
+    #${ID} .${p("row")} {
+      padding: 5px 0;
+    }
+    #${ID} .${p("row")} + .${p("row")} {
+      border-top: 1px solid #37474f;
+    }
+    #${ID} .${p("row")} > strong,
+    #${ID} .${p("row")} > span {
+      color: #fff;
+      font-size: 12px;
+      font-family: Consolas, 'Courier New', Courier, Monaco, monospace;
+      vertical-align: baseline;
+      line-height: 1.4;
+    }
+    #${ID} .${p("row")} > strong {
+      font-weight: bold;
     }
     </style>
-    ```;
+    `;
 
-    $close.id = className("close");
+    $close.id = p("close");
     $close.addEventListener("click", removePanel, false);
 
     if ($title) $panel.appendChild(createRowElement("Title : ", $title.textContent));
